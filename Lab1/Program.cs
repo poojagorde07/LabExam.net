e044e1b1c077d1caa343691b3197a9591c23bb58
poojagorde07 <138903319+poojagorde07@users.noreply.github.com>
2023-07-16 13:33:45 +0530

    Add files via upload

---
+++ Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace Lab1
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            string filePath = "mytest.txt";
+            string fileContent = "Hello and Welcome\n\nIt is the first content\n\nof the text file mytest.txt";
+
+            // Create the text file and write content to it
+            File.WriteAllText(filePath, fileContent);
+
+            // Read the content of the text file
+            string fileContentRead = File.ReadAllText(filePath);
+
+            // Display the content of the text file
+            Console.WriteLine("Here is the content of the file mytest.txt:\n");
+            Console.WriteLine(fileContentRead);
+        }
+    }
+}
